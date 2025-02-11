5b58b59c0e9da5356a896de1cb429f5fa44dabcb
enzoramos597 <enzo.j3.ramos@gmail.com>
2025-02-11 19:19:36 -0300

    Error en el ordenar solo agrega pero no ordena

--- TP3/index.mjs
+++ TP3/index.mjs
@@ -1,6 +1,15 @@
-import { leerSuperheroes } from "./utils.mjs";
+import { leerSuperheroes, agregarSuperheroes } from "./utils.mjs";
 
 //Leer y mostrar la lista de superheroes ordenada
-const superheroes = leerSuperheroes('./superheroes.txt');
+//esta es la primera
+//const superheroes = leerSuperheroes('./superheroes.txt');
+const archivoOriginal = './superheroes.txt';
+const archivoNuevos = './agregarSuperheroes.txt';
+
+// Agregar nuevos superheroes
+agregarSuperheroes(archivoOriginal, archivoNuevos);
+
+//Leer y mostrar la lista Actualizada de superheroes ordenada
+const superheroes= leerSuperheroes(archivoOriginal);
 console.log('Superheroes ordenados:');
 console.log(superheroes);
